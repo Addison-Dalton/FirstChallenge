2e51feeeea524fcc814d024257f5c9c4e9723c59
Addison Dalton <zarch14@gmail.com>
2018-02-09 17:58:48 -0500

    Add project files.

---
+++ FirstChallenge/Controllers/ComicBookController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FirstChallenge.Models;
+using System.Web.Mvc;
+using System.Net;
+
+namespace FirstChallenge.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        // GET: ComicBook
+        public ActionResult Index()
+        {
+            var comicBooks = Models.ComicBookManager.GetComicBooks();
+
+            return View(comicBooks);
+        }
+
+        // GET: ComicBook/Details
+        public ActionResult Details(int? comicBookId)
+        {
+            if(comicBookId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComicBook comicBook = Models.ComicBookManager.GetComicBooks().Find(p => p.ComicBookId == comicBookId);
+            if(comicBook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comicBook);
+        }
+    }
+}
